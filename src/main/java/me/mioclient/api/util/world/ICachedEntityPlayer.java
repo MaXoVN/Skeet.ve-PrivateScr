68bc15644d351962857e05034f479d9a0937c23a
YuneTou$$ <nguyenquyet0034@gmail.com>
2024-07-07 17:34:21 +0700

    Initial commit

---
+++ src/main/java/me/mioclient/api/util/world/ICachedEntityPlayer.java
@@ -0,0 +1,11 @@
+package me.mioclient.api.util.world;
+
+public interface ICachedEntityPlayer {
+    float getResistanceMultiplier();
+
+    float getBlastMultiplier();
+
+    float getTotalArmourValue();
+
+    float getArmourToughness();
+}
